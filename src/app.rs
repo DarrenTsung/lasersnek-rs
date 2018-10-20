734e2c24bc9402c4dc080ccea8b51596e0a25a84
Darren Tsung <darren@onesignal.com>
2018-10-20 16:25:36 -0700

    Add initial app

---
+++ src/app.rs
@@ -0,0 +1,28 @@
+use amethyst::prelude::*;
+use amethyst::renderer::{Event, KeyboardInput, VirtualKeyCode, WindowEvent};
+
+pub(crate) struct App;
+
+impl<'a, 'b> State<GameData<'a, 'b>> for App {
+    fn handle_event(&mut self, _: StateData<GameData>, event: Event) -> Trans<GameData<'a, 'b>> {
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => Trans::Quit,
+                _ => Trans::None,
+            },
+            _ => Trans::None,
+        }
+    }
+
+    fn update(&mut self, data: StateData<GameData>) -> Trans<GameData<'a, 'b>> {
+        data.data.update(&data.world);
+        Trans::None
+    }
+}
